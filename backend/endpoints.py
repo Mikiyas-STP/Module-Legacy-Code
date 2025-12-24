85bd4b8c6ced4ce922e1c17926f9cf132f0b7c49
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-12-24 20:19:37 +0000

    update rebloomcounter and rebloom endpoints/methods

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -233,11 +233,39 @@ def home_timeline():
 
     return jsonify(sorted_blooms)
 
-
 def user_blooms(profile_username):
     user_blooms = blooms.get_blooms_for_user(profile_username)
     user_blooms.reverse()
     return jsonify(user_blooms)
+
+def update_rebloom_counter(bloom_id):
+    try:
+        id_int = int(bloom_id)
+    except ValueError:
+        return make_response((f"Invalid bloom id", 400))
+    blooms.update_rebloom_counter(id_int)
+    return jsonify(
+        {
+            "success": True,
+        }
+    )
+
+@jwt_required()
+def send_rebloom():
+    user = get_current_user()
+    bloom_id = request.json["id"]
+    try:
+        id_int = int(bloom_id)
+    except ValueError:
+        return make_response((f"Invalid bloom id", 400))
+    blooms.add_rebloom(sender=user, id=id_int)
+
+    return jsonify(
+        {
+            "success": True,
+        }
+    )
+
 
 
 @jwt_required()
